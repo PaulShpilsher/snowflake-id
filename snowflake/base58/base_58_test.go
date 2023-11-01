683a76d35527955bff9b93ea6b553f9e1ac39ab6
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-01 13:09:01 +0300

    add roundtrip integrity test

--- snowflake/base58/base_58_test.go
+++ snowflake/base58/base_58_test.go
@@ -1,14 +1,16 @@
 package base58_test
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/PaulShpilsher/snowflake-id/snowflake/base58"
 )
 
 func TestEncodeInvalidInput(t *testing.T) {
 	_, err := base58.Encode(-1)
-	if err != nil {
+	if err == nil {
 		t.Fatal("expected to fail")
 	}
 }
@@ -24,9 +26,6 @@ func TestDecodeInvalidInput(t *testing.T) {
 }
 
 func TestEncoderValidAlphabet(t *testing.T) {
-
-	validCharacters := []byte("123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ")
-
 	for i := 0; i < 10000; i++ {
 		s, err := base58.Encode(int64(i))
 		if err != nil {
@@ -38,9 +37,35 @@ func TestEncoderValidAlphabet(t *testing.T) {
 		}
 
 		for _, x := range []byte(s) {
-			if !contains(x, validCharacters) {
+			if !contains(x, []byte(base58.Alphabet)) {
 				t.Fatalf("invalid encoded string: %s", s)
 			}
+		}
+	}
+}
+
+func TestEncodeDecodeRoundtrip(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixMicro()))
+	for i := 0; i < 100000; i++ {
+
+		var n int64
+		if i%2 == 0 {
+			n = r.Int63()
+		} else {
+			n = int64(r.Int31())
+		}
+		s, err := base58.Encode(n)
+		if err != nil {
+			t.Fatalf("encoding [%d] error: %v", n, err)
+		}
+
+		m, err := base58.Decode(s)
+		if err != nil {
+			t.Fatalf("decoding [%s] error: %v", s, err)
+		}
+
+		if n != m {
+			t.Fatalf("failed roundtrip: [%d] (%s) != [%d]", n, s, m)
 		}
 	}
 }
